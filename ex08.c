dcc8d2b2b860eea7df144e6f7e15c3a9e3d9a356
Yağmur Sayın <yagmur232367@gmail.com>
2025-12-06 14:37:41 +0300

    done8

--- ex08.c
+++ ex08.c
@@ -1,1 +1,21 @@
+ #include <stdio.h>
+ int main() {
+    int numbers[10];
+    int i;
+    int sum = 0;
+    float average;
+    for(i = 0; i<10; i++){
+        printf("Enter number %d: ", i+1);
+        scanf("%d", &numbers[i]);
+        sum += numbers[i];
+    }
+    average = sum / 10.0;
+    printf("Numbers entered: ");
+    for(i=0; i<10; i++){
+        printf("%d", numbers[i]);
 
+    }
+    printf("\nSum: %d", sum);
+    printf("\nAverage: %.2f\n", average);
+    return 0;
+ }

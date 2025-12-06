3dc8b179a342fa8f345f2ee2b270b6ed9b0f85ae
Yağmur Sayın <yagmur232367@gmail.com>
2025-12-06 14:47:32 +0300

    done9

--- ex09.c
+++ ex09.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main() {
+     int number;
+     int count = 0;
+     int sum = 0;
+     float average;
+
+    printf("Enter numbers (-1 to stop):\n");
+
+    while (1) {
+        printf("Number: ");
+        scanf("%d", &number);
+   
+        if (number == -1) {
+            break;
+        }
+
+        sum += number;
+        count++;
+    }
+
+    printf("Total Sum: %d\n", sum);
+
+ 
+    if (count > 0) {
+        average = (float)sum / count;
+        printf("Count: %d\n", count);
+        printf("Average: %.2f\n", average);
+    } else {
+        printf("No numbers entered.\n");
+    }
+
+    return 0;
+}

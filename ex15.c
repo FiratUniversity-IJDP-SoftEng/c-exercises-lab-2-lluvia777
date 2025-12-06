fc3702ac536d430cc4316fab498dac5fe110198a
Yağmur Sayın <yagmur232367@gmail.com>
2025-12-06 16:45:31 +0300

    Done15

--- ex15.c
+++ ex15.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main() {
+    int i, j, space;
+    int n = 5;
+
+    for (i = 1; i <= n; i++) {
+        for (space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    for (i = n - 1; i >= 1; i--) {
+        for (space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        for (j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

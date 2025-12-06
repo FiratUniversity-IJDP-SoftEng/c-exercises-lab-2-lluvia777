4e77ad821fd375bd33e0c635b633b4dbe6c202a0
Yağmur Sayın <yagmur232367@gmail.com>
2025-12-06 14:22:54 +0300

    done7

--- ex07.c
+++ ex07.c
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+int main() {
+    int i;
+    for (i = 1; i <= 10; i++) {
+        printf("%d \t %d \t %d \n", (i * 5), (100 - ((i - 1) * 10 + 1)), i);
+    }
+    return 0;
+}

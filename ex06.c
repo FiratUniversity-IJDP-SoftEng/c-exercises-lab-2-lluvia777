61cebdd7e86de842f9cd81524ed98b2edf89d4aa
lluvia777 <yagmur232367@gmail.com>
2025-12-06 14:03:43 +0300

    Implement number storage and printing program
    Add a program that stores and prints numbers from 99 to 51 decrementing by 3.

--- ex06.c
+++ ex06.c
@@ -1,1 +1,23 @@
 
+#include <stdio.h>
+
+int main() {
+int numbers[20];
+int i ;
+int count = 0;
+for (i=99; i>50; i = i-3){
+    numbers[count] = i;  
+    count++;         
+}
+for (i = 0; i<count; i++) {
+    printf("%d", numbers[i]);
+
+    if(i<count-1){
+        printf(", ");
+
+    }
+}
+printf("\n");
+    return 0;
+}
+

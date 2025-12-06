6434426ff831bd84678208a07f3001b9531ee576
Yağmur Sayın <yagmur232367@gmail.com>
2025-12-06 15:00:27 +0300

    done10

--- ex10.c
+++ ex10.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int i;
+    int j;
+
+    for (i = 1; i <= 10; i++) {
+        for (j = 1; j <= 10; j++) {
+            printf("%d\t", i * j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
